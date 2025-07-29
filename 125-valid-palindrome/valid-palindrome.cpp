03e68d947201d4838f9d4dfcefc29c115011ffb4
Sonu Gupta <sonugupta411093@gmail.com>
2025-07-29 23:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (79.72%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-         string cleaned = "";
-        for (char c : s) {
-            if (isalnum(c)) {
-                cleaned += tolower(c);
-            }
-        }
-        int left = 0, right = cleaned.length() - 1;
+         int left = 0;
+        int right = s.length() - 1;
         while (left < right) {
-            if (cleaned[left] != cleaned[right]) {
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             left++;
