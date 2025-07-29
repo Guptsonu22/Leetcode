c0435352885a7ea61e6ea9c641794a4cfe6c85c0
Sonu Gupta <sonugupta411093@gmail.com>
2025-07-29 23:35:43 +0530

    Time: 7 ms (6.58%) | Memory: 10 MB (58.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-         int left = 0;
+        int left = 0;
         int right = s.length() - 1;
         while (left < right) {
             while (left < right && !isalnum(s[left])) left++;
