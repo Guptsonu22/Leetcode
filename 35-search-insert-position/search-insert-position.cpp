1e50332f6a4314d8f6c8774b2a27c4d508411c19
Sonu Gupta <sonugupta411093@gmail.com>
2025-07-31 16:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (5.86%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,7 +4,8 @@ public:
          int low = 0, high = nums.size() - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) return mid;
+            if (nums[mid] == target) 
+            return mid;
             else if (nums[mid] < target) low = mid + 1;
             else high = mid - 1;
         }
