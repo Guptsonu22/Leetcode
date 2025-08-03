fcd63edcb46f9160c7bf15c5d5f736d5c6a599fd
Sonu Gupta <sonugupta411093@gmail.com>
2025-08-03 10:18:01 +0530

    Time: 6 ms (79.31%) | Memory: 185.6 MB (42.46%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.cpp
@@ -1,46 +1,46 @@
-class Solution {
-public:
-    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-        unordered_map<int, int> mp;
-        int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == x) {
-                count++;
-                mp[count] = i;  
-            }
-        }
-        vector<int> ans;
-        for (int i : queries) {
-            if (mp.find(i) == mp.end()) {
-                ans.push_back(-1);
-            } else {
-                ans.push_back(mp[i]); 
-            }
-        }
-        return ans;
-    }
-};
-
-
-
-
 // class Solution {
 // public:
 //     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-//         vector<int> positions;
+//         unordered_map<int, int> mp;
+//         int count = 0;
 //         for (int i = 0; i < nums.size(); i++) {
 //             if (nums[i] == x) {
-//                 positions.push_back(i);
+//                 count++;
+//                 mp[count] = i;  
 //             }
 //         }
 //         vector<int> ans;
-//         for (int q : queries) {
-//             if (q <= positions.size()) {
-//                 ans.push_back(positions[q - 1]);
-//             } else {
+//         for (int i : queries) {
+//             if (mp.find(i) == mp.end()) {
 //                 ans.push_back(-1);
+//             } else {
+//                 ans.push_back(mp[i]); 
 //             }
 //         }
 //         return ans;
 //     }
 // };
+
+
+
+
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        vector<int> positions;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == x) {
+                positions.push_back(i);
+            }
+        }
+        vector<int> ans;
+        for (int q : queries) {
+            if (q <= positions.size()) {
+                ans.push_back(positions[q - 1]);
+            } else {
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
